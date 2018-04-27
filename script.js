0b2ce22c6ea5ad8243440cc39fda5affd9ef4dc8
jacegummersall <jacegummersall@gmail.com>
2018-04-27 07:54:39 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,72 +1,44 @@
- Result
-EDIT ON
-     function setupTypewriter(t) {
-        var HTML = t.innerHTML;
+document.addEventListener('DOMContentLoaded',function(event){
+    // array with texts to type in typewriter
+    var name = ["Jace Gummersall"];
 
-        t.innerHTML = "";
+    // type one text in the typwriter
+    // keeps calling itself until the text is finished
+    function typeWriter(text, i, fnCallback) {
+        // check if text isn't finished yet
+        if (i < (text.length)) {
+            // add next character to h1
+            document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
 
-        var cursorPosition = 0,
-            tag = "",
-            writingTag = false,
-            tagOpen = false,
-            typeSpeed = 100,
-        tempTypeSpeed = 0;
-
-        var type = function() {
-        
-            if (writingTag === true) {
-                tag += HTML[cursorPosition];
-            }
-
-            if (HTML[cursorPosition] === "<") {
-                tempTypeSpeed = 0;
-                if (tagOpen) {
-                    tagOpen = false;
-                    writingTag = true;
-                } else {
-                    tag = "";
-                    tagOpen = true;
-                    writingTag = true;
-                    tag += HTML[cursorPosition];
-                }
-            }
-            if (!writingTag && tagOpen) {
-                tag.innerHTML += HTML[cursorPosition];
-            }
-            if (!writingTag && !tagOpen) {
-                if (HTML[cursorPosition] === " ") {
-                    tempTypeSpeed = 0;
-                }
-                else {
-                    tempTypeSpeed = (Math.random() * typeSpeed) + 50;
-                }
-                t.innerHTML += HTML[cursorPosition];
-            }
-            if (writingTag === true && HTML[cursorPosition] === ">") {
-                tempTypeSpeed = (Math.random() * typeSpeed) + 50;
-                writingTag = false;
-                if (tagOpen) {
-                    var newSpan = document.createElement("span");
-                    t.appendChild(newSpan);
-                    newSpan.innerHTML = tag;
-                    tag = newSpan.firstChild;
-                }
-            }
-
-            cursorPosition += 1;
-            if (cursorPosition < HTML.length - 1) {
-                setTimeout(type, tempTypeSpeed);
-            }
-
-        };
-
-        return {
-            type: type
-        };
+            // wait for a while and call this function again for next character
+            setTimeout(function() {
+                typeWriter(text, i + 1, fnCallback)
+            }, 100);
+        }
+        // text finished, call callback if there is a callback function
+        else if (typeof fnCallback == 'function') {
+            // call callback after timeout
+            setTimeout(fnCallback, 700);
+        }
     }
 
-    var typer = document.getElementById('typewriter');
+// start a typewriter animation for a text in the name array
+function StartTextAnimation(i) {
+    if (typeof name[i] == 'undefined'){
+        setTimeout(function() {
+            StartTextAnimation(0);
+        }, 20000);
+    }
+    // check if name[i] exists
+    else if (i < name[i].length) {
+        // text exists! start typewriter animation
+        typeWriter(name[i], 0, function(){
+            // after callback (and whole text has been animated), start next text
+            StartTextAnimation(i + 1);
+        });
+    }
+}
 
-    typewriter = setupTypewriter(typewriter);
-
-    typewriter.type();
+// start the text animation
+StartTextAnimation(0);
+});
