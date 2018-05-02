edfd8baeaecc9fc0310cabd645e0a28772d9485d
jacegummersall <jacegummersall@gmail.com>
2018-05-02 15:45:58 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,44 +1,50 @@
 document.addEventListener('DOMContentLoaded',function(event){
-    // array with texts to type in typewriter
-    var name = ["Jace Gummersall"];
 
-    // type one text in the typwriter
-    // keeps calling itself until the text is finished
+    var words = ["Welcome, I'm", "Jace Gummersall"];
+
     function typeWriter(text, i, fnCallback) {
-        // check if text isn't finished yet
-        if (i < (text.length)) {
-            // add next character to h1
-            document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
 
-            // wait for a while and call this function again for next character
-            setTimeout(function() {
-                typeWriter(text, i + 1, fnCallback)
-            }, 100);
+        if (i < (text.length)) {
+            if(text == "Welcome, I'm"){
+                document.getElementById("type-intro1").innerHTML = text.substring(0, i+1) +'<span id="letter" aria-hidden="true"></span>';
+                document.getElementById("letter").style.borderRight = ".05em solid #000000";
+                if (text[i] == ","){
+                    setTimeout(function() {
+                        typeWriter(text, i + 1, fnCallback)
+                    }, 1000);
+                }
+                else {
+                    setTimeout(function() {
+                        typeWriter(text, i + 1, fnCallback)
+                    }, 100);
+                }
+            }
+            else if (text == "Jace Gummersall"){
+                document.getElementById("type-intro2").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+                setTimeout(function() {
+                    typeWriter(text, i + 1, fnCallback)
+                }, 100);
+            }
+
         }
-        // text finished, call callback if there is a callback function
         else if (typeof fnCallback == 'function') {
-            // call callback after timeout
+            document.getElementById("letter").style.borderRight = "";
             setTimeout(fnCallback, 700);
         }
     }
 
-// start a typewriter animation for a text in the name array
-function StartTextAnimation(i) {
-    if (typeof name[i] == 'undefined'){
-        setTimeout(function() {
-            StartTextAnimation(0);
-        }, 20000);
+    function StartTextAnimation(i) {
+        if (typeof words[i] == 'undefined'){
+            setTimeout(function() {
+                StartTextAnimation(0);
+            }, 20000);
+        }
+        else if (i < words[i].length) {
+            typeWriter(words[i], 0, function(){
+                StartTextAnimation(i + 1);
+            });
+        }
     }
-    // check if name[i] exists
-    else if (i < name[i].length) {
-        // text exists! start typewriter animation
-        typeWriter(name[i], 0, function(){
-            // after callback (and whole text has been animated), start next text
-            StartTextAnimation(i + 1);
-        });
-    }
-}
 
-// start the text animation
-StartTextAnimation(0);
+    StartTextAnimation(0);
 });
